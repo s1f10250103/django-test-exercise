0d1b0196655f366d54a36e59b65a049d03a5052d
s1f10250103 <s1f102501035@iniad.org>
2026-06-25 13:49:05 +0900

    lintエラーの対応2

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
+from django.utils.timezone import make_aware
 from todo.models import Task
+
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        task = Task(title=request.POST['title'],
-        due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task = Task(
+            title=request.POST['title'],
+            due_at=make_aware(parse_datetime(request.POST['due_at']))
+        )
         task.save()
 
     if request.GET.get('order') == 'due':
@@ -15,8 +18,7 @@ def index(request):
     else:
         tasks = Task.objects.order_by('-posted_at')
 
-    context={
-        'tasks':tasks
+    context = {
+        'tasks': tasks
     }
-    return render(request,'todo/index.html',context)
-
+    return render(request, 'todo/index.html', context)
