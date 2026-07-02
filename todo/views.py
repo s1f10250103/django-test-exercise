cf85c3a08dcd290bc8a241f76bdadb0a625482b6
iniad <iniad@saitoreito.localdomain>
2026-07-02 14:09:35 +0900

    更新機能の追加

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from django.utils.dateparse import parse_datetime
 from django.utils.timezone import make_aware
