56d63c4a4d034c229f7a48d81632bca1a8176711
1F10250017 <s1F102500179@iniad.org>
2026-07-09 14:27:54 +0900

    delete-testを修正

--- todo/views.py
+++ todo/views.py
@@ -1,55 +1,54 @@
-from django.shortcuts import render, redirect
 from django.http import Http404
+from django.shortcuts import redirect, render
 from django.utils.dateparse import parse_datetime
 from django.utils.timezone import make_aware
+
 from todo.models import Task
 
 
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         task = Task(
-            title=request.POST['title'],
-            due_at=make_aware(parse_datetime(request.POST['due_at']))
+            title=request.POST["title"],
+            due_at=make_aware(parse_datetime(request.POST["due_at"])),
         )
         task.save()
 
-    if request.GET.get('order') == 'due':
-        tasks = Task.objects.order_by('due_at')
+    if request.GET.get("order") == "due":
+        tasks = Task.objects.order_by("due_at")
     else:
-        tasks = Task.objects.order_by('-posted_at')
+        tasks = Task.objects.order_by("-posted_at")
 
-    context = {
-        'tasks': tasks
-    }
-    return render(request, 'todo/index.html', context)
+    context = {"tasks": tasks}
+    return render(request, "todo/index.html", context)
 
-def detail(request,task_id):
+
+def detail(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
-        raise Http404('Task does not exist')
+        raise Http404("Task does not exist")
 
-    context = {
-        'task' : task,
-    }
-    return render(request, 'todo/detail.html', context)
+    context = {"task": task}
+    return render(request, "todo/detail.html", context)
+
 
 def update(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    if request.method == 'POST':
-        task.title = request.POST['title']
-        task.due_at = make_aware(parse_datetime(request.POST['due_at']))
-        task.save()
-        return redirect(detail, task_id)
 
-    context = {
-        'task': task
-    }
+    if request.method == "POST":
+        task.title = request.POST["title"]
+        task.due_at = make_aware(parse_datetime(request.POST["due_at"]))
+        task.save()
+        return redirect("detail", task_id=task_id)
+
+    context = {"task": task}
     return render(request, "todo/edit.html", context)
+
 
 def delete(request, task_id):
     try:
@@ -58,4 +57,4 @@ def delete(request, task_id):
         raise Http404("Task does not exist")
 
     task.delete()
-    return redirect(index)
+    return redirect("index")
